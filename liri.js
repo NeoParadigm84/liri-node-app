4796e681fed40f35f1326a270439aab23b772377
CJ Hubbard <mysticriver84@gmail.com>
2019-07-18 17:36:41 -0400

    spotify function

--- liri.js
+++ liri.js
@@ -29,3 +29,62 @@ for (var i = 3; i < query.length; i++) {
 array.splice(-1); //Get rid of last plus sign, if left errors caused
 var finalSearch = array.join(""); //Search query joined together to form string for any query below
 
+// node liri.js concert-this
+
+function concertMe() {
+    if (finalSearch === "") {
+        console.log('\n')
+        console.log("No Artist entered. Please enter an Artist")
+        console.log('\n')
+    } else {
+        axios.get("https://rest.bandsintown.com/artists/" + finalSearch + "/events?app_id=codingbootcamp").then(
+        function (response) {
+           if(response.data.length <= 0) {
+               console.log("No info for this Artist")
+           }else {
+            for(var i=0; i < response.data.length; i++) {
+
+                var currData = `\n
+    Venue: ${response.data[i].venue.name}
+    Location: ${response.data[i].venue.city + ", " + response.data[0].venue.region}
+    Event Date: ${moment(response.data[i].datetime).format('LL')}
+            `
+            console.log(currData)
+            }
+           }
+           
+            dataLog(currData)
+        }
+    );
+    }
+}
+
+// node liri.js spotify-this-song
+
+function spotifyIt() {
+
+    if (finalSearch === "") {
+        finalSearch = "ace+of+base+the+sign"
+    }
+
+    spotify.search({
+        type: 'artist,track',
+        query: finalSearch
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log('\n')
+
+        var currData = `\n
+    Artist: ${data.tracks.items[0].artists[0].name}
+    Track: ${data.tracks.items[0].name}
+    Preview: ${data.tracks.items[0].preview_url}
+    Album: ${data.tracks.items[0].album.name}
+            `
+            console.log(currData)
+            dataLog(currData)
+
+    });
+}
+
