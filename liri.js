3cd917a15a6663d2798dd092a2b39ebe918d061a
CJ Hubbard <mysticriver84@gmail.com>
2019-07-18 21:11:43 -0400

    added read me image file and images

--- liri.js
+++ liri.js
@@ -3,8 +3,14 @@ require("dotenv").config();
 var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
-// const log = console.log;
-// log("hello world")
+
+const chalk = require('chalk');
+const log = console.log;
+log(chalk.red.bgYellow("HELLO WORLD I'm LIRI! I’m SIRI’s 15th cousin. I’m your interactive assistant. \nPlease choose a category and type its command as listed below. \nThen type what you want to search for. \nThen leave the rest to me. Look out SIRI!"));
+log(chalk.white.bgRed("MOVIE INFO \nCOMMAND: movie-this \nSEARCH: Movie Title \nEXAMPLE COMMAND: liri.js movie-this Rambo"));
+log(chalk.white.bgGreen("CONCERT INFO \nCOMMAND: concert-this \nSEARCH: Artist \nEXAMPLE COMMAND: liri.js concert-this Kanye West"));
+log(chalk.white.bgBlue("CONCERT INFO \nCOMMAND: spotify-this-song \n SEARCH: Song Title \n EXAMPLE COMMAND: liri.js spotify-this-song Old Town Road"));
+log(chalk.white.bgMagenta("RANDOM SELECTION \nCOMMAND: do-what-it-says \n SEARCH: Song Title \n EXAMPLE COMMAND: liri.js do-what-it-says"));
 //Spotify keys
 const env = process.env;
 
@@ -30,7 +36,7 @@ for (var i = 3; i < query.length; i++) {
 array.splice(-1); //plus sign error fix
 var finalSearch = array.join(""); //Search query joined together to form string for any query below
 
-//Googled switch statement to determine argument type (concert-this, movie-this, etc.)
+//Googled switch statements used them to determine argument type (concert-this, movie-this, etc.)
 switch (type) {
     case 'concert-this':
         concertMe()
@@ -45,7 +51,7 @@ switch (type) {
         itSays()
         break;
     default:
-        console.log("No type value found");
+        console.log(chalk.red.bgYellow("WHAT WOULD YOU LIKE TO SEARCH FOR?"));
 }
 
 
@@ -72,8 +78,8 @@ function spotifyIt() {
     Preview: ${data.tracks.items[0].preview_url}
     Album: ${data.tracks.items[0].album.name}
             `
-            console.log(currData)
-            dataLog(currData)
+        console.log(currData)
+        dataLog(currData)
 
     });
 }
@@ -87,24 +93,24 @@ function concertMe() {
         console.log('\n')
     } else {
         axios.get("https://rest.bandsintown.com/artists/" + finalSearch + "/events?app_id=codingbootcamp").then(
-        function (response) {
-           if(response.data.length <= 0) {
-               console.log("No info for this Artist")
-           }else {
-            for(var i=0; i < response.data.length; i++) {
+            function (response) {
+                if (response.data.length <= 0) {
+                    console.log("No info for this Artist")
+                } else {
+                    for (var i = 0; i < response.data.length; i++) {
 
-                var currData = `\n
+                        var currData = `\n
     Venue: ${response.data[i].venue.name}
     Location: ${response.data[i].venue.city + ", " + response.data[0].venue.region}
     Event Date: ${moment(response.data[i].datetime).format('LL')}
             `
-            console.log(currData)
+                        console.log(currData)
+                    }
+                }
+
+                dataLog(currData)
             }
-           }
-           
-            dataLog(currData)
-        }
-    );
+        );
     }
 }
 
@@ -118,7 +124,7 @@ function movieThis() {
 
     axios.get("http://www.omdbapi.com/?t=" + finalSearch + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
-        
+
             var currData = `\n
     Title: ${response.data.Title}
     Released: ${response.data.Year}
@@ -134,51 +140,50 @@ function movieThis() {
         }
     );
 
-    
 }
 
 // node liri.js do-what-it-says
 
 function itSays() {
-    fs.readFile("random.txt", "utf8", function(error, data) {
+    fs.readFile("random.txt", "utf8", function (error, data) {
 
         if (error) {
-          return console.log(error);
+            return console.log(error);
         }
 
         var dataArr = data.split(",");
-      
+
         finalSearch = dataArr[1];
         spotifyIt()
-      });
+    });
 }
 
 //saves user input to log.txt
 
-var logQuery = query.splice(0,2)
-logQuery =  "\n" + query.join(" ") + "\n"
+var logQuery = query.splice(0, 2)
+logQuery = "\n" + query.join(" ") + "\n"
 console.log(logQuery)
 
-fs.appendFile("log.txt", logQuery, function(err) {
+fs.appendFile("log.txt", logQuery, function (err) {
 
     if (err) {
-      console.log(err);
+        console.log(err);
     } else {
-      console.log("Log Updated");
+        console.log(chalk.red.bgYellow("AS YOU COMMAND!"));
     }
-  
-  });
+
+});
 
 //saves user imput result to log.txt
 
 function dataLog(data) {
-    fs.appendFile("log.txt", data, function(err) {
+    fs.appendFile("log.txt", data, function (err) {
 
         if (err) {
-          console.log(err);
+            console.log(err);
         } else {
-          console.log("Log Updated");
+            console.log(chalk.red.bgYellow("GOT WHAT YOU ARE LOOKING FOR! TAKE THAT SIRI! NOW I JUST NEED A VOICE...SIGH!"));
         }
-      
-      });
-  }
+
+    });
+}
